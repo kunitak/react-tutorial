f4bfe33686b5ff47ce7a18449c5d47056bd15770
kunitak <kunitak@solutionware.jp>
2015-10-11 11:43:48 +0900

    day2

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,10 +1,44 @@
 var React = require('react');
 
+//ヘッダの定義
+var Header = React.createClass({
+  render: function(){
+    return (
+      <h1>ヘッダです</h1>
+    );
+  }
+});
+
+//ボディの定義
+var Body = React.createClass({
+  render: function(){
+    return (
+      <h1>ボディです</h1>
+    );
+  }
+});
+
+//フッタの定義
+var Footer = React.createClass({
+  render: function(){
+    return (
+      <h1>フッタです</h1>
+    );
+  }
+});
+
+//コンポーネントを一つにまとめる
 var Index = React.createClass({
   render:function(){
-  	return (
-  		<p>hoge</p>
-  	);
+    return (
+      <div>
+        <Header/>
+        <hr/>
+        <Body/>
+        <hr/>
+        <Footer/>
+      </div>
+    );
   }
 });
 
