70806de8ecc04b9facc9976dfbbee3274ff3a885
kunitak <kunitak@solutionware.jp>
2015-10-18 10:21:45 +0000

    react-router

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,17 +1,60 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var History = ReactRouter.History;
 var ReactDOM = require('react-dom');
 var Header = require('./views/header.jsx');
 var Body = require('./views/body.jsx');
+var UserBox = require('./views/userbox.jsx');
 var Footer = require('./views/footer.jsx');
 
-//classNameでcssを指定
 var Index = React.createClass({
+  render: function(){
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var Top = React.createClass({
+  mixins: [ History ],
+
+  handleSubmit:function(e){
+    e.preventDefault();
+    /* ログイン処理 */
+
+    //ポータルページへ
+    this.history.pushState(null, '/portal');
+  },
+  render:function(){
+    return (
+      <div>
+        <div className="main">
+          <h1>ログイン</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input placeholder="userid"/>
+            <input placeholder="password"/>
+            <div style={{textAlign:"cener"}}>
+              <button type="submit">ログイン</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
+var Main = React.createClass({
   render:function(){
     return (
       <div>
         <Header/>
         <div className="main">
-          <Body/>
+          {this.props.children}
         </div>
         <Footer/>
       </div>
@@ -19,7 +62,18 @@ var Index = React.createClass({
   }
 });
 
+var Routes = (
+  <Route path="/" component={Index}>
+    <IndexRoute component={Top}/>
+    <Route path="/top" component={Top}/>
+    <Route path="/portal" component={Main}>
+      <IndexRoute component={Body}/>
+      <Route path="/userbox" component={UserBox}/>
+    </Route>
+  </Route>
+);
+
 ReactDOM.render(
-  <Index />,
+  <Router>{Routes}</Router>,
   document.getElementById('content')
 );
--- client/scripts/views/header.jsx
+++ client/scripts/views/header.jsx
@@ -1,11 +1,29 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var History = ReactRouter.History;
+var Link = ReactRouter.Link;
 
 //ヘッダの定義
 var Header = React.createClass({
+  mixins: [ History ],
+  
+  handleClick: function(e){
+  	/* ログアウト処理 */
+    
+    //ログイン画面へ
+    this.history.pushState(null, '/');
+  },
   render: function(){
     return (
       <header>
-        <h1>ヘッダです</h1>
+        <div style={{position:"absolute", margin: "-15px 0px"}}>
+          <h1>ヘッダです</h1>
+        </div>
+        <div style={{position:"relative", textAlign:"right", paddingTop: "30px"}}>
+          <Link to="/portal" style={{paddingRight: "5px"}}>ポータル</Link>
+          <Link to="/userbox" style={{paddingRight: "5px"}}>ユーザーリスト</Link>
+          <button onClick={this.handleClick}>ログアウト</button>
+        </div>
         <hr/>
       </header>
     );
